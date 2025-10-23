b996157e4d5b09e200a7b253f60a01b0a7c5b182
DESKTOP-GIU52QC\JINWOO <pjw1480@naver.com>
2025-10-23 23:03:51 +0900

    [build] 프로젝트 초기 세팅

---
+++ src/test/java/com/synerge/order101/Order101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synerge.order101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Order101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
