5493b7284ff5f7b3bdd72dacddfbef28e9509cb4
sangwon <chosw0410@naver.com>
2025-11-03 19:27:50 +0900

    [feat] shipment entity

--- backend/src/main/java/com/synerge/order101/shipment/model/entity/Shipment.java
+++ backend/src/main/java/com/synerge/order101/shipment/model/entity/Shipment.java
@@ -1,7 +1,12 @@
 package com.synerge.order101.shipment.model.entity;
 
+import com.synerge.order101.order.model.entity.StoreOrder;
+import com.synerge.order101.outbound.model.entity.OutBound;
+import com.synerge.order101.store.model.entity.Store;
 import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -11,5 +16,32 @@ public class Shipment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long shipmentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "outbound_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private OutBound outbound;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_order_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private StoreOrder storeOrder;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    private enum Status{
+        WAITING, IN_TRANSIT, DELIVERED
+    }
 
 }
