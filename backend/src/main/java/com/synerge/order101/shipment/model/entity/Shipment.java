13a97cba8f224c16a24022f07b16a884d1cf47b1
sangwon5579 <81066249+sangwon5579@users.noreply.github.com>
2025-11-04 12:20:17 +0900

    Feature/8 shipment (#13)
    * [feat] application.yml ignore
    
    * [feat] shipment entity
    
    * [feat] 배송 상태 변경 구현
    
    * [feat] 배송 상태 스케줄러
    
    * [feat] enum은 글로벌로 관리
    
    * [feat] 스케줄러 및 상태 변경 log
    
    * [fix] shipmentStatus

--- backend/src/main/java/com/synerge/order101/shipment/model/entity/Shipment.java
+++ backend/src/main/java/com/synerge/order101/shipment/model/entity/Shipment.java
@@ -1,7 +1,13 @@
 package com.synerge.order101.shipment.model.entity;
 
+import com.synerge.order101.common.enums.ShipmentStatus;
+import com.synerge.order101.order.model.entity.StoreOrder;
+import com.synerge.order101.outbound.model.entity.OutBound;
+import com.synerge.order101.store.model.entity.Store;
 import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -11,5 +17,29 @@ public class Shipment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long shipmentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "outbound_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private OutBound outbound;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_order_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private StoreOrder storeOrder;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = true)
+    private ShipmentStatus shipmentStatus;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = true)
+    private LocalDateTime updatedAt;
+
+
 
 }
