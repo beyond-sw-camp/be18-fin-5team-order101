386fe3101dbbc6a9f2645147b067a2cf320dc242
Lee Jin Gu <sbeom11@naver.com>
2025-11-04 11:50:31 +0900

    Update CategoryLevel.java

--- backend/src/main/java/com/synerge/order101/product/model/entity/CategoryLevel.java
+++ backend/src/main/java/com/synerge/order101/product/model/entity/CategoryLevel.java
@@ -1,7 +1,17 @@
 package com.synerge.order101.product.model.entity;
 
 public enum CategoryLevel {
-    대,
-    중,
-    소
+    LARGE("대"),
+    MEDIUM("중"),
+    SMALL("소");
+
+    private final String label;
+
+    CategoryLevel(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
