f079d130a91740702329e4db96f62ac60f19a5c7
kyounggg <dbwlsdbrud1@naver.com>
2025-11-05 14:46:24 +0900

    [feat] 엔티티 수정

--- backend/src/main/java/com/synerge/order101/inbound/model/entity/InBoundDetail.java
+++ backend/src/main/java/com/synerge/order101/inbound/model/entity/InBoundDetail.java
@@ -1,8 +1,12 @@
 package com.synerge.order101.inbound.model.entity;
 
 
+import com.synerge.order101.product.model.entity.Product;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -12,5 +16,20 @@ public class InBoundDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long inboundDetailId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inbound_id", nullable = false)
+    private InBound inboundId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Product productId;
+
+    @Column(name = "received_qty", nullable = false)
+    private Integer receivedQuantity;
+
+    @Column(columnDefinition = "DATETIME(6)")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
--- backend/src/main/java/com/synerge/order101/warehouse/model/entity/WarehouseInventory.java
+++ backend/src/main/java/com/synerge/order101/warehouse/model/entity/WarehouseInventory.java
@@ -1,8 +1,12 @@
 package com.synerge.order101.warehouse.model.entity;
 
 
+import com.synerge.order101.product.model.entity.Product;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -12,4 +16,22 @@ public class WarehouseInventory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long inventoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouseId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product productId;
+
+    @Column(name = "on_hand_qty", nullable = false)
+    private Integer onHandQuantity;
+
+    @Column(name = "safety_qty")
+    private Integer safetyQuantity;
+
+    @Column(columnDefinition = "DATETIME(6)")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
