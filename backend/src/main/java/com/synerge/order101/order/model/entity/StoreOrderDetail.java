4814303e7310c1a873b81e827a5d30b9420a76db
sangwon5579 <81066249+sangwon5579@users.noreply.github.com>
2025-11-05 11:45:22 +0900

    [feat] StoreOrder Entity (#27)

--- backend/src/main/java/com/synerge/order101/order/model/entity/StoreOrderDetail.java
+++ backend/src/main/java/com/synerge/order101/order/model/entity/StoreOrderDetail.java
@@ -1,27 +1,68 @@
-package com.synerge.order101.order.model.entity;
+//package com.synerge.order101.order.model.entity;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//
+//@Getter
+//@Entity
+//@Table(name = "store_order_detatil")
+//public class StoreOrderDetail {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long storeOrderDetailId;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "store_order_id", nullable = false)
+//    private StoreOrder storeOrderId;
+//
+//    @Column
+//    private Double orderQTY;
+//
+//    @Column
+//    private Double unitPrice;
+//
+//
+//}
 
+
+package com.synerge.order101.order.model.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Getter
 @Entity
-@Table(name = "store_order_detatil")
+@NoArgsConstructor
+@Table(name = "store_order_detail")
 public class StoreOrderDetail {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_order_detail_id")
     private Long storeOrderDetailId;
 
-    @ManyToOne
-    @JoinColumn(name = "store_order_id", nullable = false)
-    private StoreOrder storeOrderId;
+    @ManyToOne(fetch = LAZY) @JoinColumn(name="store_order_id") StoreOrder storeOrder;
 
-    @Column
-    private Double orderQTY;
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
 
-    @Column
-    private Double unitPrice;
+    @Column(name = "order_qty", nullable = false, precision = 15, scale = 3)
+    private BigDecimal orderQty;
 
+    @Column(name = "unit_price", precision = 15, scale = 2)
+    private BigDecimal unitPrice;
 
+    @Column(name = "amount", precision = 15, scale = 2)
+    private BigDecimal amount;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
+
